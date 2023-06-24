adadf77c376ce0455fa33fb166fa2efe948f50a6
Tomas <tomas.kanka@fs.cvut.cz>
2023-06-24 15:19:43 +0200

    Implementing custom hook 'useLocalStorage'

---
+++ q
@@ -0,0 +1,11 @@
+[33mcommit 876691a784e266da8d8bf511c093ce9c92a63327[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Tomas <tomas.kanka@fs.cvut.cz>
+Date:   Tue Jun 20 08:19:57 2023 +0200
+
+    Markup of the NoteForm component created
+
+[33mcommit d6689a19d9f34edc9053ec2c1549686a495af13a[m
+Author: Tomas <tomas.kanka@fs.cvut.cz>
+Date:   Tue Jun 20 07:55:25 2023 +0200
+
+    Initial setup. React browser's  routes setted up
--- src/components/NoteFrom/NoteForm.tsx
+++ src/components/NoteFrom/NoteForm.tsx
@@ -1,39 +1,69 @@
 import { Form, Stack, Row, Col, Button } from 'react-bootstrap';
 import CreatableReactSelect from 'react-select/creatable'
 import { Link } from 'react-router-dom'
+import { useState, useRef, FormEvent } from 'react';
+import { NoteData, Tag } from '../../App';
 
-const NoteForm: React.FC = () => {
+
+interface NoteFormProps{
+  onSubmit: (data: NoteData) => void
+}
+
+const NoteForm: React.FC<NoteFormProps> = ({ onSubmit }) => {
+
+  const titleRef = useRef<HTMLInputElement>(null)
+  const markdownRef = useRef<HTMLTextAreaElement>(null)
+
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+  function handleSubmit(e: FormEvent){
+    e.preventDefault()
+
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: []
+    })
+  }
+
   return ( 
     <>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Stack gap={4}>
           <Row>
             <Col>
               <Form.Group controlId='title'>
                 <Form.Label>Title</Form.Label>
-                <Form.Control required />
+                <Form.Control ref={titleRef} required />
               </Form.Group>
             </Col>
             <Col>
               <Form.Group controlId='tags'>
                 <Form.Label>Tags</Form.Label>
-                <CreatableReactSelect isMulti />
-                  
-
+                <CreatableReactSelect 
+                  value={selectedTags.map(tag => {
+                    return { label: tag.label, value: tag.id } // This is the shape which the CreatebleReactSelect expects
+                  })}
+                  onChange={tags => {
+                    setSelectedTags(tags.map(tag => {
+                      return { label: tag.label, id: tag.value} // Converting back to our shape of the Tag type
+                    }))
+                  }}
+                isMulti />
               </Form.Group>
             </Col>
           </Row>
           <Row>
-          <Col>
-            <Form.Group controlId='markdown'>
-              <Form.Label>Note</Form.Label>
-              <Form.Control as="textarea" rows={10} />
-            </Form.Group>
+            <Col>
+              <Form.Group controlId='markdown'>
+                <Form.Label>Note</Form.Label>
+                <Form.Control ref={markdownRef} as="textarea" rows={10} />
+              </Form.Group>
             </Col>
           </Row>
           <Stack direction="horizontal" gap={3} className='justify-content-end'>
             <Button type='submit' variant='primary'>Save</Button>
-            <Link to=".."> {/* Brings user one step back in the history */}
+            <Link to=".."> {/* Allows the user to go back to the previous page they visited in their browsing history */}
               <Button type='button' variant='outline-secondary'>Cancel</Button>
             </Link>
           </Stack>
